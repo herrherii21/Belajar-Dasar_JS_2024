db0c879d341c2c285555dbf4da0c97a067305894
Heri <97837359+herrherii21@users.noreply.github.com>
2024-06-02 15:28:08 +0700

    Add files via upload

---
+++ Belajar Dasar Pemrograman JavaScript 2024/CoffeeMachine/index.js
@@ -0,0 +1,12 @@
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
+
+import { coffeeStock, isCoffeeMachineReady } from './state.js';
+ 
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
